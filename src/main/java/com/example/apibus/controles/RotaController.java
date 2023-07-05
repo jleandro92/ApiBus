79e265d929756cd100d2f9f15bc789b9c11342ca
jleandro92 <jleandro0802@gmail.com>
2023-07-05 19:13:18 -0300

    TableUser

--- src/main/java/com/example/apibus/controles/RotaController.java
+++ src/main/java/com/example/apibus/controles/RotaController.java
@@ -3,7 +3,6 @@ package com.example.apibus.controles;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.apibus.entidade.Rota;
