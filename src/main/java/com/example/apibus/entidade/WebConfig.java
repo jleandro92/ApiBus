f7d6558dd23c01b574566331445a01502b2edbc8
Dev_Martinz <Martiinz.742@gmail.com>
2023-07-29 12:51:54 -0300

    HttpMessageConverter

--- src/main/java/com/example/apibus/entidade/WebConfig.java
+++ src/main/java/com/example/apibus/entidade/WebConfig.java
@@ -1,9 +1,13 @@
 package com.example.apibus.entidade;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
@@ -16,5 +20,13 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("*")
                 .allowedHeaders("*");
     }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter(objectMapper);
+        converters.add(converter);
+    }
+    
 }
 
