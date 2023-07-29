e66bb67ee8652de1cab0e650bbf38c5f01f27e39
Dev_Martinz <Martiinz.742@gmail.com>
2023-07-29 11:55:48 -0300

    CrossOrigin Adicionado

--- src/main/java/com/example/apibus/repositorys/ParadaRepository.java
+++ src/main/java/com/example/apibus/repositorys/ParadaRepository.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.example.apibus.entidade.Parada;
 
 @RepositoryRestResource(collectionResourceRel = "parada", path = "parada" )
+@CrossOrigin(origins = "*")
 public interface ParadaRepository extends JpaRepository<Parada, Long>{
 
     List<Parada> findByparadaNome(@Param("paradaNome") String name);
--- src/main/java/com/example/apibus/repositorys/UsuarioRepository.java
+++ src/main/java/com/example/apibus/repositorys/UsuarioRepository.java
@@ -5,10 +5,12 @@ import com.example.apibus.entidade.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 //NewTable
 @RepositoryRestResource(collectionResourceRel = "usuario", path = "usuario")
+@CrossOrigin(origins = "*")
 public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
     List<Usuario> findBynomeUser (@Param("nomeUser") String name);
 }
